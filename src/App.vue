8d24f4aac8e1d4bc2957e704c806bd2329c63d3b
Yoshiki <rrrtcdish2@gmail.com>
2019-03-09 17:18:20 +0900

    add pug & add vuetify & create home

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+  #app
+    v-toolbar
+      v-toolbar-title Portfolio
+      v-toolbar-items
+        v-toolbar-side-icon
+        v-toolbar-side-icon
+        v-toolbar-side-icon
+    router-view
+    .background-image
+      img(src="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?cs=srgb&dl=beauty-bloom-blue-67636.jpg&fm=jpg")
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="sass">
+#app
+  font-family: "Avenir", Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+
+.background-image
+  display: flex
+  position: fixed
+  left: 0
+  top: 25%
+  img
+    width: 50%
+    height: 50%
 </style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/src/stylus/app.styl';
+Vue.use(Vuetify);
+//# sourceMappingURL=vuetify.js.map
